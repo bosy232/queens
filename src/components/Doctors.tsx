1ff2cc9dabef3e4c0594e409dc78590ab7e43b65
bosy232 <tarekalashry59@gmail.com>
2024-11-22 20:33:42 +0200

    Add files via upload

--- src/components/Doctors.tsx
+++ src/components/Doctors.tsx
@@ -4,19 +4,19 @@ import DoctorCard from './DoctorCard';
 const Doctors: FC = () => {
   const doctors = [
     {
-      name: 'Dr. Sherif El Aitany',
-      specialty: 'Chief of Surgery',
-      image: 'https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?auto=format&fit=crop&w=300&q=80',
-    },
-    {
-      name: 'Dr. Tarek Abdel Gawad',
-      specialty: 'Head of Pediatrics',
-      image: 'https://images.unsplash.com/photo-1559839734-2b71ea197ec2?auto=format&fit=crop&w=300&q=80',
-    },
-    {
-      name: 'Dr. Sarah Thompson',
+      name: 'Dr. Mohamed Yehia',
       specialty: 'Obstetrics & Gynecology',
-      image: 'https://images.unsplash.com/photo-1594824476967-48c8b964273f?auto=format&fit=crop&w=300&q=80',
+      image: 'https://i.ytimg.com/vi/6B7OoUcNl-A/maxresdefault.jpg',
+    },
+    {
+      name: 'Dr. Sherif Yehia',
+      specialty: 'Obstetrics & Gynecology',
+      image: 'https://i.ytimg.com/vi/54KG-sag-Hs/hqdefault.jpg',
+    },
+    {
+      name: 'Dr. Amr Helmy',
+      specialty: 'Obstetrics & Gynecology',
+      image: 'https://i.ytimg.com/vi/_A_sSjifPTM/maxresdefault.jpg',
     },
   ];
 
